b896060e0ac9069acad7221736b7b8f0e5cca231
Milton Bravo <miltonbravo96@gmail.com>
2023-11-08 23:11:21 -0600

    Se agrega forma expect

--- test/asserts_styles.spec.ts
+++ test/asserts_styles.spec.ts
@@ -33,4 +33,37 @@ describe("Estilos de assert", () => {
         assert.property(id, "color");
         assert.propertyVal(id, "color", "azul");
     });
+
+    it("Forma expect", () => {
+        //arrange
+        const nombre: string = "Johnson";
+        const edad: number = 25;
+        const tareas: Array<string> = ["tarea", "cocinar", "lavar", "estudiar"];
+        const numSuerte: Array<number> = [34, 12, 567, 9, 874, 1000];
+        const id: Object = { "nombre": "Johnson", "edad": 25, "color": "azul" };
+
+        //act
+
+        //assert
+        expect(nombre).to.be.ok;
+        expect(nombre).to.be.a("string");
+        expect(nombre).to.be.equal("Johnson");
+        
+        expect(edad).to.be.ok;
+        expect(edad).to.be.a("number");
+        expect(edad).to.be.equal(25);
+
+        expect(tareas).to.be.ok;
+        expect(tareas).to.be.a("array");
+        expect(tareas).to.include("lavar");
+
+        expect(numSuerte);
+        expect(numSuerte).to.be.a("array");
+        expect(numSuerte).to.not.include(1);
+        
+        expect(id).to.be.ok;
+        expect(id).to.be.a("object");
+        expect(id).to.haveOwnProperty("color");
+        expect(id).to.have.property("color", "azul");
+    });
 });
