979b25086764f95e8717087c875820d1a807b4ce
Milton Bravo <miltonbravo96@gmail.com>
2023-11-08 23:38:03 -0600

    Se agrega la manera spy

---
+++ test/spy_stub_mock.spec.ts
@@ -0,0 +1,44 @@
+import { expect } from "chai";
+import sinon, { SinonSpy } from "sinon";
+import Calculadora from "../src/calculadora";
+
+describe.only("spy, stub y mock", () => {
+  let calc: Calculadora;
+  let spy: SinonSpy;
+
+  before(() => {
+    calc = new Calculadora();
+  });
+
+  describe("Tests para suma", () => {
+    it("Debe sumar dos numeros y devolver la suma de esos numeros", () => {
+      //arrange
+      spy = sinon.spy(calc, "suma");
+
+      //act
+      const suma = calc.suma(3, 8);
+
+      //assert
+      expect(suma).to.be.equal(11);
+      expect(spy.calledOnceWith(3,8)).to.be.true;
+
+      spy.restore();
+    });
+  });
+
+  describe("Tests para resta", () => {
+    it("Debe restar dos numeros y devolver la rsta de esos numeros", () => {
+      //arrange
+      spy = sinon.spy(calc, "resta");
+
+      //act
+      const suma = calc.suma(4, 2);
+
+      //assert
+      expect(suma).to.be.equal(2);
+      expect(spy.calledOnceWith(4,2)).to.be.true;
+
+      spy.restore();
+    });
+  });
+});
