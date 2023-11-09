41e8ab27a689364593f73c768c869d3aedd13c6b
Milton Bravo <miltonbravo96@gmail.com>
2023-11-08 22:13:53 -0600

    Se agrega clase calculadora

---
+++ src/calculadora.ts
@@ -0,0 +1,20 @@
+class Calculadora {
+  suma(a: number, b: number): number {
+    return a + b;
+  }
+
+  resta(a: number, b: number): number {
+    return a - b;
+  }
+
+  multiplicacion(a: number, b: number): number {
+    return a * b;
+  }
+
+  division(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error("Division por cero");
+    }
+    return a / b;
+  }
+}
